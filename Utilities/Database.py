2f669fcbb96e2795b8789639553ccc0d8c1a2699
tirthyme <34771530+tirthyme@users.noreply.github.com>
2020-02-08 09:27:03 +0530

    Added Template styling

--- Utilities/Database.py
+++ Utilities/Database.py
@@ -1,13 +1,16 @@
 import pymysql
 
 class DB:
-    db = pymysql.connect(host='localhost',
+        def make_connection(self):
+            try:
+               db = pymysql.connect(host='localhost',
                              user='root',
                              password='',
                              db='UMC',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
-
-    def getConnection():
-        return connection
-       
+            except Exception as error:
+                print(error)
+            return db   
+            
+           
--- app.py
+++ app.py
@@ -5,12 +5,20 @@ from Utilities import Database
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    db = Database.DB.db
+def hello(): 
+    db = Database.DB.make_connection(Database.DB())
     if(db):
         print ("Database Successful")
-    return 'Hello World!'
+    cur = db.cursor()
+    cur.execute("select COUNT(uname),count(id) from user_master")
+    print(cur.description)
+    context = cur.fetchall() 
+    print(type(context))
+    print(context)
+    cur.close()
+    return render_template('index.html',context = context)
 
-
-
+@app.route('/register')
+def register():
+    return render_template("index.html")
     
