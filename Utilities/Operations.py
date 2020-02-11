fbee82c44c8b56448975f36d284f7aa63d446c1d
tirthyme <34771530+tirthyme@users.noreply.github.com>
2020-02-11 16:00:12 +0530

    Template Edits

---
+++ Utilities/Operations.py
@@ -0,0 +1,5 @@
+from Database import DB
+
+class Operations():
+    def filter_email(self,email):
+        return True
--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 from Utilities import Database
-
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+csrf.init_app(app)
+app.config.update(
+    SECRET_KEY = 'you-will-never-guess'
+)
 
 @app.route('/')
 def hello(): 
@@ -20,5 +25,26 @@ def hello():
 
 @app.route('/register')
 def register():
-    return render_template("index.html")
+    return render_template("userregistration.html")
     
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("page-login.html")
+    elif request.method == "POST":
+            db = Database.DB.make_connection(Database.DB())
+            cur = db.cursor()
+            q = "select * from user_master where email = %s and password = %s"
+            print((request.form.get('email') + " 00 " + (request.form.get('password'))))
+            cur.execute(q,(request.form.get('email'), (request.form.get('password'))))
+            if cur.rowcount == 0: 
+                context = {}
+                context["error"] = "Wrong Credentials try again"
+                return render_template('page-login.html',context = context)
+            cur = cur.fetchone()
+            print(cur)
+            return redirect(url_for('admin'))
+
+@app.route('/admin',methods=['GET'])
+def admin():
+    return render_template('utilities/admin_panel.html')
